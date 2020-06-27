143258e29cef82c50a3131571996ee4e495644a7
RealVkram <ajayiadegbemi@gmail.com>
2020-06-27 13:49:49 +0100

    implement the bubble sort algorithm

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//BUBBLE SORT ALGORITHM
+//INSERT SORT ALGORITHM
 
 let arr = [10, 5, 3, 8, 2, 3.5, 6, 3,  4, 7, 9, 1]
 
@@ -13,4 +13,21 @@ function insertSort( nums ) {
       }
     }
     return nums
+  }
+
+  //BUBBLE SORT ALGORITHMS
+  
+  const bubbleSort = (num) => {
+    let swapped;
+    do {
+      swapped = false;
+      //loop the entire array
+      for (let i = 0; i < num.length; i++) {
+       if(num[i] > num[i + 1]) {
+          [num[i], num[i + 1]] = [num[i + 1], num[i]]
+          swapped = true
+        }
+      }
+      
+    } while (swapped) return num
   }
